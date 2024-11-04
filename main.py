da963637cc305afef41a856b7ff730613f523a89
H.MALAVAL <hugo.malaval@cpe.fr>
2024-11-04 14:13:39 +0100

    création fichiers python

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, Label, Button, Canvas, Menu,StringVar
+
+# Définition des variables globales
+largeur = 1200
+hauteur = 700
+
+#création d'une fenetre principale 
+fenetre_principale = Tk()
+fenetre_principale.minsize(largeur+150, hauteur+50)
+fenetre_principale.title("Space Invaders")
+
