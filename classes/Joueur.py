c9898b1b110560a4ce287833c6e4788dfaf7adef
monneret <martinmonneret1@gmail.com>
2024-11-04 17:46:26 +0100

    commit

--- classes/Joueur.py
+++ classes/Joueur.py
@@ -5,11 +5,14 @@ from PIL import Image, ImageTk
 # Création de la class joueur
 class Joueur:
     # Composée de l'apparence du joueur, du score du joueur et de ses points de vie
-    def __init__(self, canvas, x, y, score, vie, size = 30):
+    def __init__(self, canvas, x, y, score, vie, size = 30, speed=5):
 
         self.score = score
         self.vie = vie
         self.canvas = canvas
+        self.speed = speed
+        self.x = x
+        self.y = y
         
         """image = Image.open("vaisseau.gif")
 
@@ -18,7 +21,7 @@ class Joueur:
         x_center, y_center = 650, 600
         width_top = 100  # Largeur du haut du trapèze
         width_bottom = 200  # Largeur du bas du trapèze
-        height = 100  # Hauteur du trapèze
+        height = 75  # Hauteur du trapèze
 
         # Calcul des points du trapèze
         points = [
@@ -27,14 +30,14 @@ class Joueur:
             x_center + width_bottom // 2, y_center + height // 2, # Point en bas à droite
             x_center - width_bottom // 2, y_center + height // 2  # Point en bas à gauche
         ]
-        self.vaisseau_id = canvas.create_polygon(points, fill="violet", outline="black", width=2)
+        self.vaisseau_id = canvas.create_polygon(points, fill="purple", outline="black", width=2)
                 
         
-    def move(self):
+    def move(self, dx):
         
         self.x += dx
-        self.y += dy
-        canvas.move(self.image_id, dx, dy)
+
+        self.canvas.move(self.vaisseau_id, dx, 0)
     
     def score(self):
         """Arguments: aucun
@@ -42,7 +45,7 @@ class Joueur:
         (10 points pour alien de base, 25 pour alien qui tire et 150 pour le boss final)
         Renvoi le score actualisé à chaque fois qu'il change et l'affiche sur la fenêtre principale"""
         score = 0
-        
+    
     
     def vie(self):
          vies = 3
