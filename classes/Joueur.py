2b486a50aa09821642dfa6f0aa9eec2783fefa2d
monneret <martinmonneret1@gmail.com>
2024-11-04 14:28:54 +0100

    class joueur

---
+++ classes/Joueur.py
@@ -0,0 +1,20 @@
+# MALAVAL Hugo, MONNERET Martin le 4/11/2024
+
+# Création de la class joueur
+class joueur:
+    # Composée du score du joueur et de ses points de vie
+    def __init__(self, score, vie):
+        self.score = []
+        self.vie = []
+    
+    def score(self):
+        """Arguments: aucun
+        But : calculer le score du joueur quand il tire sur un alien, selon le type d'alier
+        (10 points pour alien de base, 25 pour alien qui tire et 150 pour le boss final)
+        Renvoi le score actualisé à chaque fois qu'il change et l'affiche sur la fenêtre principale"""
+        score = 0
+        
+        if 
+    
+    def vie(self):
+         vies = 3
